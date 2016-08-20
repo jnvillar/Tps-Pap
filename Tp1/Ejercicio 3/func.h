87d6e3fa7d75bc7c3aaeeab67a93f8d2290cf736
juany360 <juaninv@outlook.com>
2016-08-20 13:32:18 -0300

    parser

--- Tp1/Ejercicio 3/func.h
+++ Tp1/Ejercicio 3/func.h
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
 
 int debug = 0;
+
 
 int sumaMax(vector<int> v){
 	int max = 0;
@@ -36,4 +39,128 @@ int sumaMax(vector<int> v){
 
 
 	return max;
+}
+
+
+void testManuales(){
+
+	vector<vector<int> > bateriaTests;
+	vector<int> resPosta;
+	vector<int> res;
+
+	vector<int> test1;
+	vector<int> test2;
+	vector<int> test3;
+	vector<int> test4;
+	vector<int> test5;
+	vector<int> test6;
+	
+	test1.push_back(2);
+	test1.push_back(-1);
+	test1.push_back(2);
+	resPosta.push_back(3);
+		
+	test2.push_back(-1);
+	test2.push_back(-1);
+	test2.push_back(-1);
+	test2.push_back(-1);
+	resPosta.push_back(0);
+	
+	test3.push_back(1);
+	test3.push_back(2);
+	test3.push_back(3);
+	test3.push_back(4);
+	test3.push_back(5);
+	test3.push_back(-1);
+	test3.push_back(-2);
+	test3.push_back(-3);
+	test3.push_back(-4);
+	test3.push_back(-5);
+	test3.push_back(1);
+	test3.push_back(2);
+	test3.push_back(3);
+	test3.push_back(4);	
+	resPosta.push_back(15);
+
+	test4.push_back(1);
+	test4.push_back(2);
+	test4.push_back(3);
+	test4.push_back(4);
+	test4.push_back(5);
+	test4.push_back(0);
+	test4.push_back(-2);
+	test4.push_back(-3);
+	test4.push_back(0);
+	test4.push_back(0);
+	test4.push_back(1);
+	test4.push_back(2);
+	test4.push_back(3);
+	test4.push_back(4);
+	test4.push_back(0);
+	test4.push_back(1);
+	test4.push_back(6);
+	resPosta.push_back(17);
+	
+	test5.push_back(1);
+	test5.push_back(-1);
+	test5.push_back(1);
+	test5.push_back(-1);
+	test5.push_back(1);
+	test5.push_back(-1);
+	test5.push_back(2);
+	resPosta.push_back(2);
+
+	test6.push_back(0);
+	test6.push_back(0);
+	test6.push_back(1);
+	test6.push_back(-1);
+	test6.push_back(1);
+	test6.push_back(-1);
+	test6.push_back(2);
+	resPosta.push_back(2);
+
+	bateriaTests.push_back(test1);
+	bateriaTests.push_back(test2);
+	bateriaTests.push_back(test3);
+	bateriaTests.push_back(test4);
+	bateriaTests.push_back(test5);
+	bateriaTests.push_back(test6);
+	
+
+	for (int i = 0; i < bateriaTests.size(); ++i)	{
+		res.push_back(sumaMax(bateriaTests[i]));
+	}	
+
+	for (int i = 0; i < res.size(); i++){
+		cout << res[i] <<  " "  << resPosta[i]  <<endl;		
+	}
+}
+
+vector<int> parser(char* file){
+	ifstream in(file);
+	vector<int> res;
+
+	if (in.is_open()){
+		
+		string str;
+		istringstream iss;	
+		int cantNum;	
+		int numero;
+
+		
+		getline(in, str);		// Parseo de la primer línea		
+		iss.str(str);		
+		iss >> cantNum;
+		cout << cantNum << endl;
+		
+		getline(in, str); 		//Agarro la fila de numeros;		
+		iss.str(str);
+		for (int i = 0; i < cantNum; ++i){
+			iss >> numero;
+			cout << numero << endl;
+			res.push_back(numero);			
+		}
+	}
+
+	return(res);
 }
--- Tp1/Ejercicio 3/main.cpp
+++ Tp1/Ejercicio 3/main.cpp
@@ -1,97 +1,17 @@
 #include "func.h"
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
 
-	vector<vector<int> > bateriaTests;
-	vector<int> resPosta;
-	vector<int> res;
-
-	vector<int> test1;
-	vector<int> test2;
-	vector<int> test3;
-	vector<int> test4;
-	vector<int> test5;
-	vector<int> test6;
-	
-	test1.push_back(2);
-	test1.push_back(-1);
-	test1.push_back(2);
-	resPosta.push_back(3);
-		
-	test2.push_back(-1);
-	test2.push_back(-1);
-	test2.push_back(-1);
-	test2.push_back(-1);
-	resPosta.push_back(0);
-	
-	test3.push_back(1);
-	test3.push_back(2);
-	test3.push_back(3);
-	test3.push_back(4);
-	test3.push_back(5);
-	test3.push_back(-1);
-	test3.push_back(-2);
-	test3.push_back(-3);
-	test3.push_back(-4);
-	test3.push_back(-5);
-	test3.push_back(1);
-	test3.push_back(2);
-	test3.push_back(3);
-	test3.push_back(4);	
-	resPosta.push_back(15);
-
-	test4.push_back(1);
-	test4.push_back(2);
-	test4.push_back(3);
-	test4.push_back(4);
-	test4.push_back(5);
-	test4.push_back(0);
-	test4.push_back(-2);
-	test4.push_back(-3);
-	test4.push_back(0);
-	test4.push_back(0);
-	test4.push_back(1);
-	test4.push_back(2);
-	test4.push_back(3);
-	test4.push_back(4);
-	test4.push_back(0);
-	test4.push_back(1);
-	test4.push_back(6);
-	resPosta.push_back(17);
-	
-	test5.push_back(1);
-	test5.push_back(-1);
-	test5.push_back(1);
-	test5.push_back(-1);
-	test5.push_back(1);
-	test5.push_back(-1);
-	test5.push_back(2);
-	resPosta.push_back(2);
-
-	test6.push_back(0);
-	test6.push_back(0);
-	test6.push_back(1);
-	test6.push_back(-1);
-	test6.push_back(1);
-	test6.push_back(-1);
-	test6.push_back(2);
-	resPosta.push_back(2);
-
-	bateriaTests.push_back(test1);
-	bateriaTests.push_back(test2);
-	bateriaTests.push_back(test3);
-	bateriaTests.push_back(test4);
-	bateriaTests.push_back(test5);
-	bateriaTests.push_back(test6);
-
-	for (int i = 0; i < bateriaTests.size(); ++i)	{
-		res.push_back(sumaMax(bateriaTests[i]));
+	//testManuales();	
+	vector<int> a = parser(argv[1]);
+	for (int i = 0; i < a.size(); ++i)
+	{
+		cout<< a[i];
 	}
 
-	for (int i = 0; i < res.size(); i++){
-		cout << res[i] <<  " "  <<resPosta[i]  <<endl;		
-	}
+
+	
 
 	
 	return 0;
