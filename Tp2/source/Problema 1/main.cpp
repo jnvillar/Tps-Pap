c2b0a33e32736bd5efd9c847777ce2bfdaf72fc9
alew <axel.lew@hotmail.com>
2016-09-27 21:26:42 -0300

    la concha de tu madre l

--- Tp2/source/Problema 1/main.cpp
+++ Tp2/source/Problema 1/main.cpp
@@ -1,97 +1,127 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <algorithm>
+#include "../grafo.cpp"
 
 using namespace std;
 
-class nodo{
-	public:
-		nodo(char tipo);
-		char tipo();
-		void agregarVecino(int vecino);
-		
-	private:
-		char queRepresento;
-		vector<int> adyacencias;
-};
-
-nodo::nodo(char tipo){
-	queRepresento = tipo;
-	vector<int> v;
-	adyacencias = v;
-}
-
-char nodo::tipo(){
-	return queRepresento;
-}
-
-void nodo::agregarVecino(int vecino){
-	adyacencias.push_back(vecino);
-}
-
-class problemaUno{
-	public:
-		problemaUno();
-		int resolver();
-		void parser();
-	private:
-		vector<nodo> nodos;
-		vector<int> posEscuelas;
-		vector<int> porAlumnos;
-
-		int cantNodos;
-		int cantAristas;
-};
-
-problemaUno::problemaUno(){
-	parser();
-}
-
-
-
-void problemaUno::parser(){
-	cin >> cantNodos;
-	cin >> cantAristas;
-
-	cout << "Nodos" << endl;
-
-	for (int i = 0; i < cantNodos; i++){
-		char caracter;
-		cin >> caracter;
-		
-		if (caracter == 'E'){
-			nodo nodoEscuela('E');
-			nodo nodoEscuela()
-			nodos.push_back(nodoEscuela);
-			posEscuelas.push_back(i);
+vector<int> bfs(Grafo graph, int nodoInical, int nodoFinal, vector< vector<int> > capacidad, vector< vector<int> > funcFlujo){
+	vector<bool> visitados(graph.cantNodos(),false);
+	vector<int> padre(graph.cantNodos(),-1);
+	queue<int> cola;
+	padre[nodoInical] = nodoInical;
+	visitados[nodoInical] = true;
+	cola.push(nodoInical);
+	while(!cola.empty()){
+		int nodo = cola.front();
+		cola.pop();
+		if (nodo == nodoFinal){
+			break;
 		}
-		if (caracter == 'A'){
-			nodo nodoAlumno('A');
-			nodos.push_back(nodoAlumno);
-			porAlumnos.push_back(i);
-		}
-		if (caracter == 'X'){
-			nodo nodoEsquina('X');
-			nodos.push_back(nodoEsquina);
+		vector<int> vecinos = graph.nodosAdyacentes(nodo);
+		for(int i = 0; i<vecinos.size(); i++){
+			if(!visitados[vecinos[i]] && capacidad[nodo][vecinos[i]]-funcFlujo[nodo][vecinos[i]]>0){
+				cola.push(vecinos[i]);
+				padre[vecinos[i]] = nodo;
+			}
 		}
 	}
 
-	cout << "Aristas" << endl;
-
-	for (int i = 0 ; i < cantAristas; i++){
-		int nod1,nod2;
-		cin >> nod1;
-		cin >> nod2;
-
-		nod1--;
-		nod2--;
-
-		nodos[nod1].agregarVecino(nod2);
-		nodos[nod2].agregarVecino(nod1);
+	vector<int> camino;
+	int nodo = nodoFinal;
+	camino.push_back(nodo);
+	while(nodo != padre[nodo]){
+		nodo = padre[nodo];
+		camino.push_back(nodo);
 	}
-} 
 
-int main(int argc, char const *argv[])
+	reverse(camino.begin(),camino.end());
+	return camino;
+}
+
+int flujoMaximo(Grafo graph, int s, int t, vector< vector<int> > capacidad){
+	
+	vector<int> ceros(graph.cantNodos(),0);
+	vector< vector<int> > funcFlujo(graph.cantNodos(),ceros);
+	while(true){
+		vector<int> caminoAumento = bfs(graph,s,t,capacidad,funcFlujo);
+		if(caminoAumento.size() == 0){
+			break;
+		}
+for (int i = 0; i < caminoAumento.size(); ++i)
 {
-	problemaUno a;
+	cout << caminoAumento[i] << "  ";
+}
+cout << endl;
+		int maxAumento = 10000;
+		for(int i = 0; i<caminoAumento.size()-1; i++){
+			int a = caminoAumento[i];
+			int b = caminoAumento[i+1];
+			maxAumento = min(maxAumento,capacidad[a][b]-funcFlujo[a][b]);
+		}
+		for(int i = 0; i<caminoAumento.size()-1; i++){
+			int a = caminoAumento[i];
+			int b = caminoAumento[i+1];
+			capacidad[a][b] += maxAumento;
+		}
+	}
+	int res = 0;
+	for(int i = 0; i<graph.cantNodos(); i++){
+		res += funcFlujo[i][t];
+	}
+	return res;
+}
+
+
+int main(){
+	int m,n;
+	cin >> n;
+	cin >> m;
+	vector<int> escuelas;
+	vector<int> alumnos;
+	for (int i = 0; i < n; i++){
+		char val;
+		cin >> val;
+		if(val == 'E'){
+			escuelas.push_back(i);
+		} else if (val == 'A'){
+			alumnos.push_back(i);
+		}
+	}
+	Grafo graph(2*n+2);			// n_in en posiciones pares, n_out en impares, nodo n = s, nodo n+1 = t
+	int cantNodos = 2*n+2;
+	
+	for(int i = 0; i<cantNodos; i+=2){
+		graph.agregarArista(i,i+1);
+	}
+	for(int i = 0; i<m; i++){
+		int desde,hasta;
+		cin >> desde;
+		cin >> hasta;
+		desde--;
+		hasta--;
+		graph.agregarArista(2*desde+1,2*hasta);
+		graph.agregarArista(2*hasta+1,2*desde);
+	}
+	for(int i = 0; i<alumnos.size(); i++){
+		graph.agregarArista(n,2*alumnos[i]);
+	}
+	for(int i = 0; i<escuelas.size(); i++){
+		graph.agregarArista(2*escuelas[i]+1,n+1);
+	}
+
+	vector<int> ceros(cantNodos,0);
+	vector< vector<int> > capacidad(cantNodos,ceros);
+	for(int i = 0; i<cantNodos; i++){
+		vector<int> vecinos = graph.nodosAdyacentes(i);
+		for(int j = 0; j<vecinos.size(); j++){
+			capacidad[i][vecinos[j]] = 1;
+		}
+	}
+
+	int res = flujoMaximo(graph,n,n+1,capacidad);
+	cout << res << endl;
 	return 0;
 }
+
--- Tp2/source/grafo.cpp
+++ Tp2/source/grafo.cpp
@@ -4,76 +4,42 @@
 
 using namespace std;
 
-class Nodo{
-	public:
-		Nodo(int n){
-			numero = n;
-			vector<Nodo*> misVecinos;
-		}
 
-		Nodo(int num, vector<Nodo*> misVecinos){
-			numero = num;
-			vecinos = misVecinos;
-		}
-		
-		void agregarVecino(Nodo &vecinoNuevo){
-			vecinos.push_back(&vecinoNuevo);
-		}
-
-		bool esVecinoDe(Nodo &nod){
-			for (int i = 0; i < vecinos.size(); i++){
-				if(vecinos[i]->getNumero() == nod.getNumero()){
-					return true;
-				}
-			}
-			return false;
-		}
-
-		int getNumero(){
-			return numero;
-		}
-
-	private:
-		vector<Nodo*> vecinos;
-		int numero;
-};
 
 
 class Grafo{
 	public:
-		Grafo(vector<Nodo*> misNodos, int nods, int aris, bool tipo){
-			esDirigido = tipo;
-			nodos = misNodos;
-			cantNodos = nods;
-			cantAristas = aris;
+		Grafo(int n){
+			for (int i = 0; i < n; ++i){
+				vector<int> vacia;
+				listaAdy.push_back(vacia);
+			}
+			cantidadNodos = n;
+			cantidadAristas = 0;
 		}
 
-		Grafo(int nods, int aris, bool tipo){
-			esDirigido = tipo;
-			cantNodos = nods;
-			cantAristas = aris;
-		}
-
-		void agregarAristaDirigida(int nod1, int nod2){
-			assert(esDirigido);
-			nodos[nod1]->agregarVecino(*nodos[nod2]);
-			cantAristas++;
-		}		 
+		 
 
 		void agregarArista(int nod1, int nod2){
-			assert(!esDirigido);
-			nodos[nod1]->agregarVecino(*nodos[nod2]);
-			nodos[nod2]->agregarVecino(*nodos[nod1]);
-			cantAristas++;
+			listaAdy[nod1].push_back(nod2);
+			cantidadAristas++;
+			return;
 		}
 
-		void agregarNodo(Nodo &nuevoNodo){
-			cantNodos++;
-			nodos.push_back(&nuevoNodo);
+		int cantNodos(){
+			return cantidadNodos;
+		}
+
+		int cantAristas(){
+			return cantidadAristas;
+		}
+
+		vector<int> nodosAdyacentes(int nodo){
+			return listaAdy[nodo];
 		}
 
 	private:
-		bool esDirigido;
-		vector<Nodo*> nodos;
-		int cantNodos,cantAristas;
+		vector< vector<int> > listaAdy;
+		int cantidadNodos;
+		int cantidadAristas;
 };
