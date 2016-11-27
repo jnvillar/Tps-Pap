f1c6564739de4ea0d4ddd551c6ed8a9200e3e961
juani <juaninv@outlook.com>
2016-11-26 21:31:58 -0300

    ahora si

--- Tp2/ej4/ej4.cpp
+++ Tp2/ej4/ej4.cpp
@@ -7,9 +7,10 @@ int main(int argc, char const *argv[])
 
 	vector<pair<int,int> > preguntas;
 	Grafo grafo = parser(preguntas);
-	vector<int> respuestas;
+	vector<int> respuestas = grafo.kosaraju();
+
 	for (int i = 0; i < preguntas.size(); i++){
-		if (respuestas[preguntas[i].first] == respuestas[preguntas[i].second]){	
+		if (respuestas[preguntas[i].first] == respuestas[preguntas[i].second]){
 			cout << "S" << endl;
 		}else{
 			cout << "N" << endl;
